14e31918f761be96b7aaf1e9ac6792ada393dd08
지은세 <jieunsse@jieunses-iMac.local>
2024-10-07 21:44:02 +0900

    Post Controller Swagger content added.

--- src/main/java/com/example/CRUD_2/controller/SaveController.java
+++ src/main/java/com/example/CRUD_2/controller/SaveController.java
@@ -3,6 +3,8 @@ package com.example.CRUD_2.controller;
 import com.example.CRUD_2.dto.SaveDTO;
 import com.example.CRUD_2.service.ContentService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -29,11 +31,27 @@ public class SaveController {
         this.contentService = contentService;
     }
 
+
+    // 스웨거 코드
     @Operation(summary = "게시글 작성", description = "사용자가 작성한 글에 대해 POST 요청 이행")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "작성 성공"),
-            @ApiResponse(responseCode = "400", description = "작성 실패")
+            @ApiResponse(responseCode = "200", description = "작성 성공",
+            content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(
+                            value = "{ \"title\": \"글 제목\",  \"content\": \"글 내용\"}"
+                    )
+            )),
+            @ApiResponse(responseCode = "400", description = "작성 실패",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples =@ExampleObject(
+                                    value = "{ \"error\": \"작성 실패\"}"
+                            )
+                    ))
     })
+
+
     @PostMapping("/save")
     public ResponseEntity<?> saveLogic(SaveDTO saveDTO) {
 
